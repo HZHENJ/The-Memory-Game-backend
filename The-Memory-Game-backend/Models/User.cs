6deff24f62c5e59e49abfead50eca27c70b9f777
HUANG ZHENJIA <E1352450@u.nus.edu>
2024-12-25 22:31:46 +0800

    Add new field called username in Model/User.cs

--- The-Memory-Game-backend/Models/User.cs
+++ The-Memory-Game-backend/Models/User.cs
@@ -2,8 +2,9 @@ namespace TheMemoryGameBackend.Models
 {
     public class User
     {
-        public int Id { get; set; } // 主键
-        public string Email { get; set; } // 用户邮箱
-        public string Password { get; set; } // 用户密码
+        public int Id { get; set; } // key
+        public string Email { get; set; } // email
+        public string Password { get; set; } // password
+        public string Username { get; set; } // username
     }
 }
