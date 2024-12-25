0784cf95cf637727a6f2f7c85aba4f3932978f0f
HUANG ZHENJIA <E1352450@u.nus.edu>
2024-12-25 16:32:53 +0800

    First commit

---
+++ The-Memory-Game-backend/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace The_Memory_Game_backend.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
