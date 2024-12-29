132f0e92e91cad755cc3f03bd5d5e4d06935d04b
HUANG ZHENJIA <E1352450@u.nus.edu>
2024-12-29 22:06:10 +0800

    .

---
+++ The-Memory-Game-backend/Migrations/20241229140347_AddUserTypeField.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace The_Memory_Game_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserTypeField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "type",
+                table: "Users",
+                newName: "Type");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Type",
+                table: "Users",
+                newName: "type");
+        }
+    }
+}
